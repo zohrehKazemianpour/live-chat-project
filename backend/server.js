4228de6c85ddc131b802d990ec36fbce1b3902ea
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2026-01-12 21:15:36 +0000

    feat: add POST /messages with input validation

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express"
 
 
 const app = express();
+app.use(express.json());
+
+
 const port = 3000;
 
 
@@ -13,6 +16,33 @@ app.get("/messages", (req,res) => {
 
 })
 
+app.post("/messages", (req, res) => {
+
+    
+    const { from, text } = req.body;
+
+    if (!from || !text || from.trim() === "" || text.trim() === "") {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: "Please provide both a name and a message.",
+        });
+    }  
+
+
+    messages.push({from,text})
+   
+   res.status(201).json({from, text});
+
+
+})
+
+
+
+
 app.listen(port, () => {
     console.log(`Quote server listening on port ${port}`);
 })
+
+
