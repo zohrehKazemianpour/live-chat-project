6d7c8c2e721c8c6e4107cd4cbdc7d02f87589454
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2026-01-20 13:00:16 +0000

    feat: add short polling to fetch new messages since last timestamp

--- backend/public/script.js
+++ backend/public/script.js
@@ -1,60 +1,58 @@
+const state = {
+  messages: [],
+};
+
 const chatForm = document.getElementById("chat-form");
+const messageList = document.getElementById("message-list");
 
+async function keepFetchingMessages() {
+  const lastMessage = state.messages[state.messages.length - 1];
+  const lastMessageTime = lastMessage ? lastMessage.timestamp : "";
 
-
-
-    const messageList = document.getElementById("message-list");
-
-
-
-    async function loadMessages() {
   try {
-    const res = await fetch("/messages");
-    const data = await res.json();
-
-    messageList.innerHTML = '';
-
-     data.forEach(msg => {
-      const div = document.createElement('div');
-      div.innerHTML = `<strong>${msg.from}:</strong> ${msg.text}`;
+    
+    const res = await fetch(`/messages?since=${lastMessageTime}`);
+    const newMessages = await res.json();
+    if (newMessages.length > 0) {
+      
+      state.messages = [...state.messages, ...newMessages];
+      
+      newMessages.forEach((msg) => {
+        const div = document.createElement("div");
+        div.innerHTML = `<strong>${msg.from}:</strong> ${msg.text}`;
         messageList.appendChild(div);
-     })
+      });
+    }
   } catch (error) {
-    messageList.innerText = "Ouch! Something went wrong!"
-    console.error("Fetch error:", error)
+    console.error("Fetch error:", error);
   }
-   
-
 }
 
 
+
 chatForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-        const userName = document.getElementById("name-input").value;
-        const userText = document.getElementById("message-input").value;
+  const userName = document.getElementById("name-input").value;
+  const userText = document.getElementById("message-input").value;
 
-      const res = await fetch ("/messages", {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ from: userName, text: userText }),
-    })
+  const res = await fetch("/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ from: userName, text: userText }),
+  });
 
-    if (res.ok) {
-        console.log("Message sent successfully!");
-        // Optional: Clear the message box so the user can type a new one
-        document.getElementById("message-input").value = "";
-        loadMessages();
-    } else {
-        const errorData = await res.json();
-        alert("Error: " + errorData.message);
-    }
+  if (res.ok) {
+    console.log("Message sent successfully!");
+    // Optional: Clear the message box so the user can type a new one
+    document.getElementById("message-input").value = "";
+    keepFetchingMessages()
+  } else {
+    const errorData = await res.json();
+    alert("Error: " + errorData.message);
+  }
 });
-    
 
-
-
-
-loadMessages();
+keepFetchingMessages()
--- backend/server.js
+++ backend/server.js
@@ -1,59 +1,34 @@
-import express from "express"
-
+import express from "express";
 
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
-
 const port = 3000;
 
+const messages = [{ from: "Zohreh", text: "Welcome to the chat!" }];
 
-const messages = [{ from: "Zohreh", text: "Welcome to the chat!"}];
-
-
-app.get("/messages", (req,res) => {
-
+app.get("/messages", (req, res) => {
   const since = req.query.since;
 
-  if (since !== undefined) {
-    const sinceIndex = parseInt(since);
-    const messageSince = messages.slice(sinceIndex + 1);
-    res.json(messageSince)
-
+  if (since) {
+    const filtered = messages.filter((m) => m.timestamp > since);
+    res.json(filtered);
   } else {
     res.json(messages);
   }
-
-})
+});
 
 app.post("/messages", (req, res) => {
-
-    
-    const { from, text } = req.body;
-
-    if (!from || !text || from.trim() === "" || text.trim() === "") {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message: "Please provide both a name and a message.",
-        });
-    }  
-
-
-    messages.push({from,text})
-   
-   res.status(201).json({from, text});
-
-
-})
-
-
-
+  const newMessage = {
+    from: req.body.from,
+    text: req.body.text,
+    timestamp: new Date().toISOString(),
+  };
+  messages.push(newMessage);
+  res.status(201).json(newMessage);
+});
 
 app.listen(port, () => {
-    console.log(`server listening on port ${port}`);
-})
-
-
+  console.log(`server listening on port ${port}`);
+});
